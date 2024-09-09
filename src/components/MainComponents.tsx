90308f1bf6c3c4071d37bf71ed103c464c599269
Gideon Ekeke <gideon.ekeke@signaladoc.com>
2024-09-09 16:24:53 +0100

    done

--- src/components/MainComponents.tsx
+++ src/components/MainComponents.tsx
@@ -150,7 +150,7 @@ export default function Component() {
 									Contact Me
 								</a>
 								<a
-									href="/public/project/Gideon's Resume.pdf"
+									href="../project/Gideon's Resume.pdf"
 									download="Gideon's Resume.pdf"
 									className='inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'>
 									Download CV
